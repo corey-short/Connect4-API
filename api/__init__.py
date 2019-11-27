ded6d3abf6e9df6292363eb0db614a16f97b04a4
Corey Short <cshort012@gmail.com>
2019-11-27 15:47:41 -0800

    feature: added errorhandler to app.

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    from .BadRequestError import BadRequestError, bad_request
+
+    @app.errorhandler(BadRequestError)
+    def bad_request_handler(error):
+        return bad_request(str(error))
+
     from .views import main
     app.register_blueprint(main)
 
